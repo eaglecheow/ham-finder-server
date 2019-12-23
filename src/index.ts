35cdc6f0383e481fc0eac1c68982ce18f492437d
Eagle Cheow <kecy6cyt@nottingham.edu.my>
2019-12-23 13:02:41 +0800

    Changed host IP

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { TCPServer, FileOption } from "./utils/TCPServer";
 
-let server = new TCPServer("127.0.0.1", 8282, {
+let server = new TCPServer("0.0.0.0", 8282, {
   outputDirectory: "/",
   outputFileName: "file_copy.jpg"
 });
